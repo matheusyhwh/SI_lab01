af601468d9469edc93d4abac050e0f3ed7e38b38
matheusyhwh <matheusyhwh@gmail.com>
2017-06-05 23:48:41 -0300

    acrescentada função minimo, que acha o elemento menor no array

--- max_and_min.py
+++ max_and_min.py
@@ -4,3 +4,10 @@ def maximo(array):
         if element > maior:
             maior = element
     return maior
+
+def minimo(array):
+    menor = array[0]
+    for element in array:
+        if element < menor:
+            menor = element
+    return menor
